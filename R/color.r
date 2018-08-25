661ed749af1642d76f63801c24a9ae39624fe95b
Mara Averick <maraaverick@gmail.com>
2018-08-25 08:10:12 -0400

    Add owl palette 🦉

--- R/color.r
+++ R/color.r
@@ -54,3 +54,32 @@ scale_color_semiotic <- scale_colour_semiotic
 #' @export
 #' @rdname scale_semiotic
 scale_fill_semiotic <- function(...) { discrete_scale("colour", "semiotic", semiotic_pal(), ...) }
+
+# colour palette based on night-owl theme
+owl_palette <- c("#5f7e97", "#addb67", "#d6deeb", "#c792ea", "#82aaff", "#7fdbca", "#f78c6c", "#7e57c2", "#ffeb95", "#ff5874")
+
+#' A ten-colour palette
+#'
+#' @export
+#' @examples
+#' library(scales)
+#' scales::show_col(owl_pal()(10))
+owl_pal <- function() { scales::manual_pal(owl_palette) }
+
+#' Discrete color & fill scales based on the owl palette
+#'
+#' See [owl_pal]().
+#'
+#' @md
+#' @inheritDotParams ggplot2::discrete_scale -expand -position
+#' @rdname scale_owl
+#' @export
+scale_colour_owl <- function(...) { discrete_scale("colour", "owl", owl_pal(), ...) }
+
+#' @export
+#' @rdname scale_owl
+scale_color_owl <- scale_colour_owl
+
+#' @export
+#' @rdname scale_owl
+scale_fill_owl <- function(...) { discrete_scale("colour", "owl", owl_pal(), ...) }
