92485063f41bbc9c6a7ae15fc1fd6fee92fbaf25
Mara Averick <maraaverick@gmail.com>
2018-08-25 07:57:07 -0400

    Add semiotic palette

--- R/color.r
+++ R/color.r
@@ -25,3 +25,32 @@ scale_color_ipsum <- scale_colour_ipsum
 #' @export
 #' @rdname scale_ipsum
 scale_fill_ipsum <- function(...) { discrete_scale("fill", "ipsum", ipsum_pal(), ...) }
+
+# colour palette from semiotic https://emeeks.github.io/semiotic/#/semiotic/
+semiotic_palette <- c("#b6a756", "#b3331d", "#00a2ce", "#4d430c", "#007190", "#d38779")
+
+#' A six-colour palette
+#'
+#' @export
+#' @examples
+#' library(scales)
+#' scales::show_col(semiotic_pal()(6))
+semiotic_pal <- function() { manual_pal(semiotic_palette) }
+
+#' Discrete color & fill scales based on the semiotic palette
+#'
+#' See [semiotic_pal]().
+#'
+#' @md
+#' @inheritDotParams ggplot2::discrete_scale -expand -position
+#' @rdname scale_semiotic
+#' @export
+scale_colour_semiotic <- function(...) { discrete_scale("colour", "semiotic", semiotic_pal(), ...) }
+
+#' @export
+#' @rdname scale_semiotic
+scale_color_semiotic <- scale_colour_semiotic
+
+#' @export
+#' @rdname scale_semiotic
+scale_fill_semiotic <- function(...) { discrete_scale("colour", "semiotic", semiotic_pal(), ...) }
